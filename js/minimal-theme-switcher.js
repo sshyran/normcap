362ea77f20f0adc4f2bf536be2d69b81fa0906ba
dynobo <dynobo@mailbox.org>
2021-11-06 15:24:47 +0100

    Lots of changes

--- js/minimal-theme-switcher.js
+++ js/minimal-theme-switcher.js
@@ -27,3 +27,38 @@ const themeSwitcher = {
 
 // Init
 themeSwitcher.init();
+
+
+const detailsToggler = {
+  onMutation(mutationList, observer) {
+    mutationList.forEach(function(mutation) {
+      if (mutation.type === 'attributes' && mutation.attributeName === 'open') {
+        let id = mutation.target.getAttribute("id");
+        if (id) {
+          history.pushState({},"", window.location.href.split('#')[0] + "#" + id);
+        }
+      }
+    })
+  },
+
+  init() {
+    // Open details if hash, adjust scroll a bit
+    const hash = document.location.hash
+    if (hash.startsWith("#faqs-") || hash.startsWith("#python-package-")) {
+      const id = hash.slice(1);
+      const detail = document.getElementById(id);
+      detail.setAttribute("open", "");
+      window.setTimeout(function(){
+        window.scrollTo(window.scrollX, window.scrollY - 100);
+      }, 10);
+    }
+
+    // Adjust hash on details open
+    const observer = new MutationObserver(this.onMutation);
+    document.querySelectorAll("details").forEach(function(detail) {
+      observer.observe(detail, {attributes: true});
+    });
+  }
+}
+
+detailsToggler.init();
